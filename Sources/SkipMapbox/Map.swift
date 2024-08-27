184a80301e9ea251a1bd030366aea23d3353dc12
John Bent <theclimbingguide@gmail.com>
2024-08-27 14:52:39 -0400

    Update compile directives

--- Sources/SkipMapbox/Map.swift
+++ Sources/SkipMapbox/Map.swift
@@ -5,7 +5,7 @@
 //  Created by John Bent on 8/27/24.
 //
 
-#if !os(macOS)
+#if SKIP || !os(macOS)
 
 import Foundation
 import SwiftUI
@@ -28,29 +28,29 @@ public struct MapView: View {
         
     }
     
+    #if !SKIP
     public var body: some View {
-        #if !SKIP
         MapboxMaps.Map(viewport: $viewport) {
             Puck2D(bearing: .heading)
                 .showsAccuracyRing(true)
         }
         .mapStyle(.outdoors)
-        #else
-        ComposeView { context in
-            com.mapbox.maps.extension.compose.MapboxMap(
-                context.modifier,
-                mapViewportState = MapViewportState().apply {
-                    setCameraOptions {
-                        zoom(2.0)
-                        center(Point.fromLngLat(-98.0, 39.5))
-                        pitch(0.0)
-                        bearing(0.0)
-                    }
-                }
-            )
-        }
-        #endif
     }
+    #else
+    @Composable override func ComposeContent(context: ComposeContext) {
+        com.mapbox.maps.extension.compose.MapboxMap(
+            context.modifier,
+            mapViewportState = com.mapbox.maps.extension.compose.animation.viewport.MapViewportState().apply {
+                setCameraOptions {
+                    zoom(2.0)
+                    center(Point.fromLngLat(-98.0, 39.5))
+                    pitch(0.0)
+                    bearing(0.0)
+                }
+            }
+        )
+    }
+    #endif
 }
 
 #endif
